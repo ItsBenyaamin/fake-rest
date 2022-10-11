cdaecf389a3c2c57bc42a8d68c39b130ed8d9efc
Benyamin Eskandari <me@benyaamin.com>
2022-10-11 19:21:23 +0330

    remove clap for the sake of development:D

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,30 @@
-use std::path::PathBuf;
-use clap::Parser;
+use tokio::{net::TcpListener, io::AsyncReadExt};
 
 mod server_config;
 mod request;
 mod response;
 
-#[derive(Parser)]
-#[command(author, version, about, long_about = Some(
-    "Fake Rest make development easier by serving a fake restApi server with given config file."
-))]
-pub struct FakeRestArgs {
-    #[arg(short, long)]
-    pub config: PathBuf
-}
+
 
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync + 'static>>;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let args = FakeRestArgs::parse();
-    let _server_config = server_config::parse_config_file(args.config).await?;
+    let listener = TcpListener::bind("127.0.0.1:7000").await?;
+    
+    loop {
+        let (mut socket, _) = listener.accept().await?;
+        let mut buf = String::new();
+        
+        tokio::spawn(async move {
+            let mut request_buf = String::new();
+            socket.read_to_string(&mut request_buf).await.unwrap();
+
+            let parsed_request = request::parse_request();
+            let response = response::get_proper_response();
+            
+        });
+    }
 
     Ok(())
 }
