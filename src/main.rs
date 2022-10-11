afd80e4938217b1071ca46174c6a2705910da15b
Benyamin Eskandari <me@benyaamin.com>
2022-10-11 15:11:58 +0330

    implement cli args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, World!");
+use std::path::PathBuf;
+use clap::Parser;
+
+mod server_config;
+mod request;
+mod response;
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = Some(
+    "Fake Rest make development easier by serving a fake restApi server with given config file."
+))]
+pub struct FakeRestArgs {
+    #[arg(short, long)]
+    pub config: PathBuf
+}
+
+type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync + 'static>>;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let args = FakeRestArgs::parse();
+    let _server_config = server_config::parse_config_file(args.config).await?;
+
+    Ok(())
 }
