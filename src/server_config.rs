dca881e2397dd29002f414f17020a59aac03147f
Benyamin Eskandari <me@benyaamin.com>
2022-10-17 23:52:14 +0330

    update to use toml

--- src/server_config.rs
+++ src/server_config.rs
@@ -2,24 +2,26 @@ use std::path::PathBuf;
 
 use serde::{Serialize, Deserialize};
 
-
+#[derive(Deserialize)]
 pub struct ServerSchema {
     pub endpoints: Vec<ServerDataSchema>
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Deserialize)]
 pub struct ServerDataSchema {
+    pub method: String,
     pub path: String,
     pub result_type: String,
+    pub result: String,
+    pub status_code: usize,
     pub headers: Vec<String>,
-    pub result_path: Option<PathBuf>,
-    pub result_string: Option<String>
+    pub queries: Option<Vec<String>>
 }
 
 
 pub async fn parse_config_file(path: PathBuf) -> tokio::io::Result<ServerSchema> {
     let content = tokio::fs::read_to_string(path).await?;
-    let parsed_to_serde: Vec<ServerDataSchema> = serde_json::from_str(&content)?;
+    let parsed_to_serde: Vec<ServerDataSchema> = toml::from_str(&content).unwrap();
     Ok(
         ServerSchema { endpoints: parsed_to_serde }
     )
