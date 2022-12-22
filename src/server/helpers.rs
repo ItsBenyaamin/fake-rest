921fd29a0334a05522b14ffc5607c3477ecbac64
Benyamin Eskandari <me@benyaamin.com>
2022-12-22 14:02:55 +0330

    add get key/value type

--- src/server/helpers.rs
+++ src/server/helpers.rs
@@ -5,14 +5,14 @@ pub fn get_key_value(content: &str, delimiter: char) -> Result<(String, String),
     let key = match split.next() {
         Some(key) => key.trim().to_string(),
         None => return Err(Error::ParsingError(
-            format!("cant parse this header: `{}`", content)
+            format!("cant parse this {}: `{}`", get_key_value_type(delimiter), content)
         ))
     };
 
     let value = match split.next() {
         Some(value) => value.trim().to_string(),
         None => return Err(Error::ParsingError(
-            format!("cant parse this header: `{}`", content)
+            format!("cant parse this {}: `{}`", get_key_value_type(delimiter), content)
         ))
     };
     Ok((key, value))
@@ -23,7 +23,7 @@ pub fn get_key_optional_value(content: &str, delimiter: char) -> Result<(String,
     let key = match split.next() {
         Some(key) => key.trim().to_string(),
         None => return Err(Error::ParsingError(
-            format!("cant parse this header: `{}`", content)
+            format!("cant parse this {}: `{}`", get_key_value_type(delimiter), content)
         ))
     };
 
@@ -32,4 +32,12 @@ pub fn get_key_optional_value(content: &str, delimiter: char) -> Result<(String,
         None => String::new()
     };
     Ok((key, value))
+}
+
+fn get_key_value_type<'a>(delimiter: char) -> &'a str {
+    if delimiter == ':' {
+        "header"
+    }else {
+        "query string"
+    }
 }
