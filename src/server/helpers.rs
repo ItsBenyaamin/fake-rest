c2e364556a509cbb7293137fcb01f9a8eccdc2c7
Benyamin Eskandari <me@benyaamin.com>
2022-12-22 13:58:26 +0330

    add get key and optional value

--- src/server/helpers.rs
+++ src/server/helpers.rs
@@ -3,17 +3,33 @@ use crate::error::Error;
 pub fn get_key_value(content: &str, delimiter: char) -> Result<(String, String), Error> {
     let mut split = content.split(delimiter);
     let key = match split.next() {
-        Some(key) => key.trim(),
+        Some(key) => key.trim().to_string(),
         None => return Err(Error::ParsingError(
             format!("cant parse this header: `{}`", content)
         ))
     };
 
     let value = match split.next() {
-        Some(value) => value.trim(),
+        Some(value) => value.trim().to_string(),
         None => return Err(Error::ParsingError(
             format!("cant parse this header: `{}`", content)
         ))
     };
-    Ok((key.to_string(), value.to_string()))
+    Ok((key, value))
+}
+
+pub fn get_key_optional_value(content: &str, delimiter: char) -> Result<(String, String), Error> {
+    let mut split = content.split(delimiter);
+    let key = match split.next() {
+        Some(key) => key.trim().to_string(),
+        None => return Err(Error::ParsingError(
+            format!("cant parse this header: `{}`", content)
+        ))
+    };
+
+    let value = match split.next() {
+        Some(value) => value.trim().to_string(),
+        None => String::new()
+    };
+    Ok((key, value))
 }
